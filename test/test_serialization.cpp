20a6b89958680ef2ded078015bfc772b7c018105
Nicolai Spohrer <nicolai@xeve.de>
2020-07-09 21:34:38 +0200

    Better CMakeLists.txt structure

---
+++ test/test_serialization.cpp
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+#include "A.h"
+#include "microbuf_debug.h"
+#include <cstdint>
+#include <iostream>
+#include <iomanip>
+#include <ios>
+
+TEST(microbuf_cpp_serialization, fixarray)
+{
+    using namespace microbuf::debug;
+
+    std::vector<uint8_t> bytes;
+
+    microbuf::append_array(bytes, 1);
+    std::vector<uint8_t> expected_bytes {0x91};
+    EXPECT_EQ(bytes, expected_bytes);
+    bytes.clear();
+
+
+    microbuf::append_array(bytes, 15);
+    expected_bytes = {0x9f};
+    EXPECT_EQ(bytes, expected_bytes);
+    bytes.clear();
+}
+
+TEST(microbuf_cpp_serialization, MessageA)
+{
+    A_struct_t a;
+    a.foo = 1.23;
+    a.bar = 4.56;
+
+    std::vector<uint8_t> expected_bytes {0x91};
+    EXPECT_EQ(a.as_bytes(), expected_bytes);
+}
