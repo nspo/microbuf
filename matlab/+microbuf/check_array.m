ee3fcb4d0b46e27962e1c2b353cf5464ea33e40e
Nicolai Spohrer <nicolai@xeve.de>
2020-07-12 13:18:15 +0200

    Cleaner MATLAB array check

--- matlab/+microbuf/check_array.m
+++ matlab/+microbuf/check_array.m
@@ -29,7 +29,11 @@ end
 if array_length <= 2^16-1
     % try array16
     if bytes(idx) == hex2dec('dc')
-        encoded_length = uint16(bytes(idx+1))*2^8 + uint16(bytes(idx+2));
+        if microbuf.is_big_endian()
+            encoded_length = typecast(bytes(idx+1:idx+2), 'uint16');
+        else
+            encoded_length = typecast(flip(bytes(idx+1:idx+2)), 'uint16');
+        end
         if encoded_length == array_length
             idx = idx+3;
             err = false;
@@ -46,9 +50,11 @@ end
 if array_length <= 2^32-1
     % try array32
     if bytes(idx) == hex2dec('dd')
-        encoded_length = uint32(bytes(idx+1))*2^24 + ...
-            uint32(bytes(idx+2))*2^16 + uint32(bytes(idx+3))*2^8 + ...
-            uint32(bytes(idx+4));
+        if microbuf.is_big_endian()
+            encoded_length = typecast(bytes(idx+1:idx+4), 'uint32');
+        else
+            encoded_length = typecast(flip(bytes(idx+1:idx+4)), 'uint32');
+        end
         if encoded_length == array_length
             idx = idx+5;
             err = false;
